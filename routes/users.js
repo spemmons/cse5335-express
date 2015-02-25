d08ff94948f399a8708b0418c47dad27cf5a66b5
spemmons <s.p.emmons@gmail.com>
2015-02-25 09:53:23 -0600

    explore routes

--- routes/users.js
+++ routes/users.js
@@ -3,6 +3,12 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
+  console.log('request=' + Object.keys(req).sort().join(', '));
+  console.log('originalurl=' + req.originalUrl);
+  console.log('baseurl=' + req.baseUrl);
+  console.log('query=');
+  for (var key in req.query)
+    console.log('- ' + key + '=' + req.query[key])
   res.send('respond with a resource');
 });
 
